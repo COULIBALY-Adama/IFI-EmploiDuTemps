363fc8d6b29bc210e99792a5fa154151418f3528
Coulibaly Adama <coulibalysadama@gmail.com>
2018-10-24 20:12:19 +0000

    configuration du fichier index

--- index.php
+++ index.php
@@ -1,57 +1,68 @@
-<?php
-$db = new PDO('mysql:host=localhost', 'root', null);
-?>
-<!doctype html>
-<html lang=en>
-<head>
-    <meta charset=utf-8>
-    <title> Docker-LAMP</title>
-
-    <style>
-        @import 'https://fonts.googleapis.com/css?family=Montserrat|Raleway|Source+Code+Pro';
-
-        body { font-family: 'Raleway', sans-serif; }
-        h2 { font-family: 'Montserrat', sans-serif; }
-        pre {
-            font-family: 'Source Code Pro', monospace;
-
-            padding: 16px;
-            overflow: auto;
-            font-size: 85%;
-            line-height: 1.45;
-            background-color: #f7f7f7;
-            border-radius: 3px;
-
-            word-wrap: normal;
-        }
-
-        .container {
-            max-width: 1024px;
-            width: 100%;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <header>
-            <img src="pictures/lamp-server.png" alt="Docker LAMP logo" />
-            <h2>Bienvenu à <a href="https://github.com/COULIBALY-Adama/IFI-lamp-Server.git" target="_blank">Docker-Lamp</a> LAMP-Server</h2>
-        </header>
-        <article>
-            <p>
-                Pour la documentation, <a href="https://github.com/COULIBALY-Adama/IFI-lamp-Server.git" target="_blank">cliquez ici</a>.
-            </p>
-        </article>
-        <section>
-            <pre>
-OS: <?php echo php_uname('s'); ?><br/>
-Apache: <?php echo apache_get_version(); ?><br/>
-MySQL Version: <?php echo $db->getAttribute( PDO::ATTR_SERVER_VERSION ); ?><br/>
-PHP Version: <?php echo phpversion(); ?><br/>
-phpMyAdmin Version: <?php echo getenv('PHPMYADMIN_VERSION'); ?><span>&nbsp;</span>
-            </pre>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>EdT-IFI</title>
+        <link rel="stylesheet" href="/assets/css/bulma.min.css">
+    </head>
+    <body>
+        <section class="hero is-medium is-info is-bold">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        Emploi du Temps de l'IFI <br>
+                    <img src="logo-ifi.png" alt="Le logo du site">
+                    </h1>
+                    <h2 class="subtitle">
+                        INSTITUT DE LA FRANCOPHONIE POUR L'INNOVATION
+                    </h2>
+                </div>
+            </div>
         </section>
-    </div>
-</body>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column">
+                        <h3 class="title is-3 has-text-centered">Environement</h3>
+                        <hr>
+                        <div class="content">
+                            <ul>
+                                <li><?php echo "Emploi du Temps" ?></li>
+                                <li><?php echo apache_get_version(); ?></li>
+                                <li>PHP <?php echo phpversion(); ?></li>
+                                <li>
+                                    <?php
+                                    $link = mysqli_connect("mysql", "root", "admin", null);
+
+/* check connection */
+                                    if (mysqli_connect_errno()) {
+                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
+                                    } else {
+                                        /* print server version */
+                                        printf("MySQL Server %s", mysqli_get_server_info($link));
+                                    }
+                                    /* close connection */
+                                    mysqli_close($link);
+                                    ?>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <h3 class="title is-3 has-text-centered"> Liens</h3>
+                        <hr>
+                        <div class="content">
+                            <ul>
+                                <li><a href="http://localhost/EdT-IFI">EdT-IFI</a></li>
+                                <li><a href="http://localhost/phpinfo.php">phpinfo()</a></li>
+                                <li><a href="http://localhost:8080">phpMyAdmin</a></li>
+                                <li><a href="http://localhost/test_db.php">Test DB Connection</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
 </html>
